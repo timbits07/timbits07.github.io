2e9842660ea3efedd7d9e175a7cc5ed3d637dcf0
timbits07 <tim.storey@yahoo.com>
2014-09-16 14:59:46 -0700

    project section complete

--- javascript/main.js
+++ javascript/main.js
@@ -67,6 +67,19 @@ $(document).ready(function(){
     })
 
   //Portfolio sections click reveals correct content
-  
+  var newHash     = '',
+   $mainContent = $('#project_div');
+
+  $('#projects').delegate('a', 'click', function() {
+    window.location.hash = $(this).attr('href');
+    return false;
+  });
+
+  // Not all browsers support hashchange
+  // For older browser support: http://benalman.com/projects/jquery-hashchange-plugin/
+  $(window).bind('hashchange', function() {
+    newHash = window.location.hash.substr(1);
+    $mainContent.load(newHash + " #project_div > *");
+  });
 
 })
